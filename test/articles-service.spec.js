7911787fe4a21605a8f13ec6e6c8ccb24046d095
kraigwilliams <krgwilliams@gmail.com>
2020-02-08 13:36:16 -0400

    get articles tests are passing

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -36,27 +36,41 @@ before(()=>{
       before(() => db('blogful_articles').truncate());
 
     
-      before(()=>{
-        return db
-        .into('blogful_articles')
-        .insert(testArticles)
-      })
   
   
       
 
   after(()=>db.destroy());
       
-
+afterEach(()=>db('blogful_articles').truncate())
   
 
  describe(`getAllArticles()`, ()=>{
-  it(`resolves all articles from 'blogful_articles' table`, ()=>{
-    return ArticlesService.getAllArticles(db)
-    .then(actual =>{
-      expect(actual).to.eql(testArticles)
+   context(`Given 'blogful_articles' has data`,()=>{
+
+    before(()=>{
+      return db
+      .into('blogful_articles')
+      .insert(testArticles)
     })
-  })
+
+    it(` getAllArticles resolves all articles from 'blogful_articles' table`, ()=>{
+      return ArticlesService.getAllArticles(db)
+      .then(actual =>{
+        expect(actual).to.eql(testArticles)
+      })
+    })
+
+
+   })
+   context(`Given 'blogful_articles' has no data`, () =>{
+     it(`getAllArticles() resolves an empty array`, ()=>{
+       return ArticlesService.getAllArticles(db)
+       .then(actual =>{
+         expect(actual).to.eql([])
+       })
+     })
+   })
 
 
  })
