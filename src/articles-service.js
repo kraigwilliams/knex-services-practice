ae91f7decfa90950e0c6944be534445f67af32fa
kraigwilliams <krgwilliams@gmail.com>
2020-02-08 13:54:35 -0400

    setup insertArtices and test passing

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,11 +1,13 @@
-'use strict';
+
 const ArticlesService ={
   getAllArticles(knex){
     return knex.select('*').from('blogful_articles');
 //return Promise.resolve('all the articles')
-  }
+  },
 
-
+insertArticle(){
+  return Promise.resolve({})
+}
 
 };
 
--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -75,6 +75,23 @@ afterEach(()=>db('blogful_articles').truncate())
 
  })
  
+ context(`Given 'blogful_articles' has no data`, ()=>{
+   
+  it(`getAllArticles() resolves an empty array`, ()=>{
+     return ArticlesService.getAllArticles(db)
+     .then(actual=>{
+       expect(actual).to.eql([])
+     })
+   })
+   it(`insertArticle() inserts a new article and resolves the new article wiht an 'id'`, ()=>{
+     const newArticle ={
+       title:'Test new title',
+       content: 'Test new content',
+       date_published: new Date('2020-01-01T00:00:00.000Z')
+     }
+     return ArticlesService.insertArticle(db, newArticle)
+   })
+ })
   
   
 
